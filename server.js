00fef9079263e49bab1f302b4e230e21505abca1
ikaera <ikaera@gmail.com>
2023-03-14 12:40:29 -0400

    add layouts.

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const express = require("express");
-const session = require("express-session");
-const routes = require("./_controllers");
+// Dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
+const session = require('express-session');
+const routes = require('./_controllers');
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/dish-routes'));
+
 const sess = {
-  secret: "Super secret secret",
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -26,5 +38,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () =>
+    console.log('Server listening on: http://localhost:' + PORT)
+  );
 });
