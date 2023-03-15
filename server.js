a84fe47a3c6a1c691a7f6230dd80f37e4bae5a92
ikaera <ikaera@gmail.com>
2023-03-15 11:29:33 -0400

    edit server.js.

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 // Dependencies
 const express = require('express');
+// Import express-handlebars
 const exphbs = require('express-handlebars');
-const path = require('path');
 const hbs = exphbs.create({});
+const path = require('path');
+// Import express-session
 const session = require('express-session');
-const routes = require('./_controllers');
 
+const routes = require('./_controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set Handlebars as the default template engine.
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/dish-routes'));
-
+// Set up sessions
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -31,9 +28,17 @@ const sess = {
 };
 
 app.use(session(sess));
+// const hbs = exphbs.create({ helpers });
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./_controllers/'));
 
 app.use(routes);
 
