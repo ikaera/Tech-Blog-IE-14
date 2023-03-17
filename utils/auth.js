cedcbea6eac89df39ff24c665d198c5bc0df80cf
ikaera <ikaera@gmail.com>
2023-03-17 14:11:17 -0400

    change logged_in to loggeIn in all routes.

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,7 @@
+const session = require('express-session');
+
 const withAuth = (req, res, next) => {
-  if (!req.session.logged_in) {
+  if (!req.session.loggedIn) {
     res.redirect('/login');
   } else {
     next();
