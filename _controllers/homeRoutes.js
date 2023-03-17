00a35edcf31c85df64cea92c6c7258d8adb6c176
ikaera <ikaera@gmail.com>
2023-03-17 02:48:38 -0400

    change _view to view.

--- _controllers/homeRoutes.js
+++ _controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const { Usertech, Commenttech, Blogtech } = require('../_models');
 const withAuth = require('../utils/auth');
 
 // GET all blogs for homepage
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const blogData = await Blogtech.findAll({
       include: [
